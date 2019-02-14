de4fd1839a9276296f3a7a9bb0fa412f70d94669
jjediny <john.jediny@gsa.gov>
2019-02-14 17:46:51 -0500

    working config template through vars

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,19 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_filebeat_is_installed(host):
+    package = host.package("filebeat")
+    assert package.is_installed
+    assert package.version.startswith("6")
+
+
+def test_filebeat_service_enabled(host):
+    service = host.service('filebeat')
+    assert service.is_enabled
+
+
+def test_filebeat_config_file_present(host):
+    config_file = host.file('/etc/filebeat/filebeat.yml')
+    assert config_file.is_file
